cd79a9ba2bd4b8cb9e3e8772aa56c10fc8315bd7
Mukesh Murugan <iammukeshm@gmail.com>
2024-03-01 22:40:59 +0530

    upload cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { CommandMenu } from "@/components/command-menu";
 import { ProjectCard } from "@/components/project-card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -17,10 +16,10 @@ export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
-        <div className="flex items-center px-4 py-8 border-b text-center">
+        <div className="flex items-center px-4 pb-6 pt-4 border-b text-center">
           <div className="flex-1 space-y-1.5">
             <h1 className="text-4xl">{RESUME_DATA.name}</h1>
-            <p className="text-pretty text-sm text-muted-foreground">
+            <p className="text-pretty text-sm">
               {RESUME_DATA.about}
             </p>
             <p className="items-center text-pretty  text-xs text-muted-foreground pb-3">
@@ -33,16 +32,21 @@ export default function Page() {
                 {RESUME_DATA.location}
               </a>
             </p>
-            <Button size={"sm"} variant={'default'} className="text-pretty  text-xs">
-              <DownloadIcon className="mr-2 h-4 w-4" /> Resume
-            </Button>
+            <div className=" print:hidden">
 
-            <Button size={"sm"} variant={'outline'} className="text-pretty  text-xs ml-1">
-              <FlameIcon className="mr-2 h-4 w-4" /> Blog
-            </Button>
+              <a href={RESUME_DATA.resume} target="_blank">
+                <Button size={"sm"} variant={'default'} className="text-pretty  text-xs">
+                  <DownloadIcon className="mr-2 h-4 w-4" /> Resume
+                </Button>
+
+              </a>
+              <Button size={"sm"} variant={'outline'} className="text-pretty  text-xs ml-1">
+                <FlameIcon className="mr-2 h-4 w-4" /> Blog
+              </Button>
+            </div>
           </div>
         </div>
-        <Section>
+        <Section className="print:hidden">
           <h2 className="text-xl">Connect</h2>
           <div className="flex gap-x-1 pt-1  text-sm text-muted-foreground print:hidden">
             {RESUME_DATA.contact.social.map((social) => (
@@ -74,10 +78,7 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 leading-none">
-                      <a className="hover:underline" href={work.link}>
-                        {work.company}
-                      </a>
-
+                      {work.company}
                       <span className="inline-flex gap-x-1">
                         {work.badges.map((badge) => (
                           <Badge
@@ -152,19 +153,6 @@ export default function Page() {
           </div>
         </Section>
       </section>
-
-      <CommandMenu
-        links={[
-          {
-            url: RESUME_DATA.personalWebsiteUrl,
-            title: "Personal Website",
-          },
-          ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
-            url: socialMediaLink.url,
-            title: socialMediaLink.name,
-          })),
-        ]}
-      />
     </main>
   );
 }
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -4,6 +4,7 @@ import { PenIcon, YoutubeIcon } from "lucide-react";
 export const RESUME_DATA = {
   name: "Mukesh Murugan",
   initials: "MM",
+  resume: "/cv.pdf",
   location: "Trivandrum, Kerala, India",
   locationLink: "https://www.google.com/maps/place/Wrocław",
   about:
@@ -53,7 +54,7 @@ export const RESUME_DATA = {
     {
       company: "CSG International",
       link: "https://parabol.co",
-      badges: ["remote"],
+      badges: ["remote", "current"],
       title: "Senior Full Stack Developer",
       start: "2021",
       end: "2024",
@@ -63,7 +64,7 @@ export const RESUME_DATA = {
     {
       company: "Allianz",
       link: "https://clevertech.biz",
-      badges: ["Remote"],
+      badges: ["remote"],
       title: "Lead Android Developer → Full Stack Developer",
       start: "2015",
       end: "2021",
@@ -136,116 +137,6 @@ export const RESUME_DATA = {
         label: "github.com",
         href: "https://jarocki.me/",
       },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
     },
   ],
 } as const;
