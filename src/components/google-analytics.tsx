73f8982cd7f43347b7ba2d26e6d066332ac1475f
Mukesh Murugan <iammukeshm@gmail.com>
2024-03-02 12:03:41 +0530

    add gtm

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -1,8 +1,9 @@
 
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 export function Analytics() {
     return (
-        <><GoogleAnalytics gaId={process.env.GA4_MEASUREMENT_ID!} /></>
+        <><GoogleAnalytics gaId={process.env.GA4_MEASUREMENT_ID!} />
+            <GoogleTagManager gtmId={process.env.GA4_MEASUREMENT_ID!} /></>
     )
 }
